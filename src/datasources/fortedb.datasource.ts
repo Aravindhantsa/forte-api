c76cd74fa9193ee832eb66c268b87f8bfbf3cc3a
aravind-tsa <aravindhan.selvamani@diversebrains.com>
2025-11-19 09:59:20 +0530

    datasource config changes

--- src/datasources/fortedb.datasource.ts
+++ src/datasources/fortedb.datasource.ts
@@ -7,11 +7,11 @@ const config = {
   name: 'fortedb',
   connector: 'mysql',
   url: '',
-  host: '127.0.0.1',
-  port: 3306,
-  user: 'root',
-  password: 'tsa_1234',
-  database: 'forte'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 };
 
 // Observe application's life cycle to disconnect the datasource when
